333678b46de97803b2a61dcc0fc9beb00aee7fc1
Cezzaribm <bernardocezarimoreira@gmail.com>
2026-06-17 15:22:08 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,10 @@
+# questão 01
+nome = str(input("Digite seu primeiro nome: "))
+sobrenome = str(input("Digite seu sobrenome: "))
+print("Bem-Vindo(a),", nome, sobrenome + "!")
+# questão 02
+frase = str(input("Digite a frase: "))
+espaco = frase.count(" ")
+print("Quantidade de espaços:", espaco)
+# questão 03
+
