97d13fcdd978ebde5cbec143083a27902dcdf4d6
Cezzaribm <bernardocezarimoreira@gmail.com>
2026-06-17 17:14:48 -0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,10 +1,27 @@
-# questão 01
+# Questão 01
 nome = str(input("Digite seu primeiro nome: "))
 sobrenome = str(input("Digite seu sobrenome: "))
 print("Bem-Vindo(a),", nome, sobrenome + "!")
-# questão 02
+
+# Questão 02
 frase = str(input("Digite a frase: "))
 espaco = frase.count(" ")
 print("Quantidade de espaços:", espaco)
-# questão 03
 
+# Questão 03
+for i in range(1, len(nome) + 1):
+    print(nome[:i])
+# Questão 04
+numero = input("Digite o número: " )
+if len(numero) == 8:
+    numero = "9" + numero
+    print("Número completo: ", numero[:5] + "-" + numero[5:])
+elif len(numero) == 9 and numero[0] == "9":
+    print("Número completo: ", numero[:5] + "-" + numero[5:])
+else:
+    print("Número inválido!")
+
+# Questão 05 terminar 
+
+print("Índices das vogais: ", )
+print("Quantidade de vogais: ", )
